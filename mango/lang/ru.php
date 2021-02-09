a8c1c7e7e7289c484aab2ea43f81e5cdeec69769
SacredV <78743826+SacredV@users.noreply.github.com>
2021-02-09 14:56:08 +0300

    Update ru.php

--- mango/lang/ru.php
+++ mango/lang/ru.php
@@ -1,13 +1,13 @@
 <?php
 
 return [
-    "name" => "Манго",
-    "fname" => "Манго2",
-    "description" => "Система телефонии MANGO",
+    "name" => "science communication",
+    "fname" => "science communication",
+    "description" => "Система телефонии science communication",
     "transfer" => "Перенаправлять при вх. звонке на ответственного",
     "createRelationship" => "Создавать контакт",
     "createDeals" => "Добавить звонок в последнюю сделку",
     "url" => "Адрес API Виртуальной АТС (call-back)",
-    "api_key" => "Уникальный код вашей АТС (из ЛК Mango Office)",
-    "api_salt" => "Ключ для создания подписи (из ЛК Mango Office)"
+    "api_key" => "Уникальный код вашей АТС (из ЛК science communication Office)",
+    "api_salt" => "Ключ для создания подписи (из ЛК science communication Office)"
 ];
